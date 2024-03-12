e241d04ee2383db274ce9e074a5e58a697efd1b6
javad71 <60092712+javad71@users.noreply.github.com>
2024-03-12 14:16:19 +0330

    update code

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
+import datetime
+import random
+import string
 import json
-from flask import Flask, render_template, request, send_file, redirect, flash
+import logging
+from flask import Flask, render_template, request, send_file, redirect, flash, jsonify, send_from_directory
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from ultralytics import YOLO
 
@@ -8,22 +13,45 @@ from ultralytics import YOLO
 try:
     with open('config.json') as f:
         config = json.load(f)
-except:
-    pass
+except Exception as e:
+    logging.error(f"Error loading config: {e}")
+    config = {}
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Configure CORS to allow all origins
 app.secret_key = config['secret_key']
-app.config['UPLOAD_FOLDER'] = 'uploads'
+upload_folder = config['upload_folder']
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+# Set up logging
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+# Load the model once during application startup
+model = YOLO('./models/wd-yolov8-small.pt')  # load a custom model
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
+def generate_random_filename():
+    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return f"{current_time}_{random_string}"
+
+
 # Endpoint for main dashboard
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    if request.method == 'GET':
+        # Load existing data
+        json_filename = './result.json'
+        with open(json_filename, 'r') as json_file:
+            response_message = json.load(json_file)
+
+        return render_template('index.html', result_data=response_message)
+
     if request.method == 'POST':
         # Check if the post request has the file part
         if 'file' not in request.files:
@@ -38,31 +66,79 @@ def main():
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
-            # Save the uploaded file
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            try:
+                # Save the uploaded file
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            # Perform your image processing here
-            processed_filename = process_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # Perform your image processing here
+                processed_filename = process_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            # Send the processed image back to the page
-            return send_file(processed_filename, as_attachment=True)
+                # Load existing data
+                json_filename = './result.json'
+                with open(json_filename, 'r') as json_file:
+                    response_message = json.load(json_file)
+
+                return render_template('index.html', result_data=response_message)
+
+            except Exception as e:
+                logging.error(f"Error processing image: {e}")
+                response_message = {'status': 'error', 'message': 'Error processing image.'}
+                return render_template('index.html', result_data=response_message)
 
     return render_template('index.html')
 
 
 def process_image(input_path):
-    # Load a model
-    model = YOLO('./models/wd-yolov8-small.pt')  # load a custom model
+    try:
+        # Predict with the model
+        results = model(input_path, conf=0.5, imgsz=640)  # predict on an image
 
-    # Predict with the model
-    results = model(input_path, conf=0.5, imgsz=640)  # predict on an image
+        # Process results list
+        result_data = []
+        for result in results:
+            filename = f'./uploads/{generate_random_filename()}.jpg'
+            result.save(filename=filename)  # save to disk
 
-    # Process results list
-    for result in results:
-        result.save(filename='./uploads/result.jpg')  # save to disk
+            labels = []
+            for box in result.boxes:
+                label = result.names[int(box.cls)]  # Get the class label
+                labels.append(label)
 
-    return input_path
+            result_info = {
+                'tags': labels,
+                'path': filename
+            }
+            result_data.append(result_info)
+
+        # JSON file path
+        json_filename = './result.json'
+
+        # Check if the JSON file exists
+        if os.path.exists(json_filename):
+            # Load existing data
+            with open(json_filename, 'r') as json_file:
+                existing_data = json.load(json_file)
+            # Append new data to the existing list
+            existing_data.extend(result_data)
+        else:
+            # If the file doesn't exist, use the new data
+            existing_data = result_data
+
+        # Save the updated results in the JSON file
+        with open(json_filename, 'w') as json_file:
+            json.dump(existing_data, json_file, indent=4)
+
+        return input_path
+    except Exception as e:
+        logging.error(f"Error processing image: {e}")
+        return None
+
+
+# Endpoint for update profile picture
+@app.route('/uploads/<filename>')
+def result_image(filename):
+    return send_from_directory(upload_folder, filename)
 
 
 if __name__ == '__main__':
